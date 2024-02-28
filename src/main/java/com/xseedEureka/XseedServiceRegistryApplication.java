512b9fee3476d45d4b74b6b01639e3748d73b011
Pranjali Suryawanshi <Pranjali Suryawanshi@DESKTOP-IR8NEVB.TCOGNITION>
2024-02-28 02:12:57 -0500

    exceptions handled and package rename

--- src/main/java/com/xseedEureka/XseedServiceRegistryApplication.java
+++ src/main/java/com/xseedEureka/XseedServiceRegistryApplication.java
@@ -1,5 +1,7 @@
 package com.xseedEureka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -7,9 +9,15 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class XseedServiceRegistryApplication {
-
+	   private static final Logger logger = LoggerFactory.getLogger(XseedServiceRegistryApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(XseedServiceRegistryApplication.class, args);
+	//	SpringApplication.run(XseedServiceRegistryApplication.class, args);
+		 try {
+	            SpringApplication.run(XseedServiceRegistryApplication.class, args);
+	        } catch (Exception e) {
+	            logger.error("Error during application startup: {}", e.getMessage(), e);
+	          
+	        }
 	}
 
 }
--- src/test/java/com/xseedEureka/registry/XseedServiceRegistryApplicationTests.java
+++ src/test/java/com/xseedEureka/registry/XseedServiceRegistryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.javatechie;
+package com.xseedEureka.registry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SwiggyServiceRegistryApplicationTests {
+class XseedServiceRegistryApplicationTests {
 
 	@Test
 	void contextLoads() {
